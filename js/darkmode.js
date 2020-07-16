839e124f4b174929c1ee5d8be281dd59abb66190
JuanCarlosRamirez <58491643+JuanCarlosRamirez-dev@users.noreply.github.com>
2020-07-15 23:51:41 -0500

    Codigo estable - logotipos funcionando

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,7 +1,35 @@
-function darkMode() {
-    localStorage.setItem('mode', (localStorage.getItem('mode') || 'dark') === 'dark' ? 'light' : 'dark');
-    localStorage.getItem('mode') === 'dark' ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
-}
+//funcion que carga el body y las imagenes dependiendo del dark mode o light mode
 document.addEventListener('DOMContentLoaded', (event) => {
-    ((localStorage.getItem('mode') || 'dark') === 'dark') ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
-})
+    ((localStorage.getItem('mode') || 'dark') === 'dark') ? (
+        document.querySelector('body').classList.add('dark'),
+        document.getElementById('logoMobileId').src = "imagenes/logo-mobile-modo-noc.svg",
+        document.getElementById('logoDesktopId').src = "imagenes/logo-desktop-modo-noc.svg"
+    ) : (
+            document.querySelector('body').classList.remove('dark'),
+            document.getElementById('logoMobileId').src = "imagenes/logo-mobile.svg",
+            document.getElementById('logoDesktopId').src = "imagenes/logo-desktop.svg"
+        );
+});
+
+//funcion que intercambia el body entre dark mode y light mode
+let darkMode = document.getElementById('darkModeId');
+darkMode.addEventListener('click', (event) => {
+    localStorage.setItem('mode', (localStorage.getItem('mode') || 'dark') === 'dark' ? 'light' : 'dark');
+    localStorage.getItem('mode') === 'dark' ? document.querySelector('body').classList.add('dark')
+        : document.querySelector('body').classList.remove('dark');
+});
+
+//funcion que intercambia el logotipo de version movil entre dark mode y light mode
+darkMode.addEventListener('click', (event) => {
+    ((localStorage.getItem('mode') || 'dark') === 'dark') ?
+        document.getElementById('logoMobileId').src = "imagenes/logo-mobile-modo-noc.svg"
+        : document.getElementById('logoMobileId').src = "imagenes/logo-mobile.svg";
+});
+
+//funcion que intercambia el logotipo de version desktop entre dark mode y light mode
+darkMode.addEventListener('click', (event) => {
+    ((localStorage.getItem('mode') || 'dark') === 'dark') ?
+        document.getElementById('logoDesktopId').src = "imagenes/logo-desktop-modo-noc.svg"
+        : document.getElementById('logoDesktopId').src = "imagenes/logo-desktop.svg";
+
+});
