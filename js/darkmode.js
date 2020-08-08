ad7bbca7fe504b82aae83d8e86ef7d46adeb092f
JuanCarlosRamirez <58491643+JuanCarlosRamirez-dev@users.noreply.github.com>
2020-08-08 18:26:07 -0500

    vermas & autocomplete

--- js/darkmode.js
+++ js/darkmode.js
@@ -42,46 +42,93 @@ darkMode.addEventListener('click', (event) => {
         : document.getElementById('searchBtnId').src = "imagenes/icon-search.svg";
 });
 
-let searchGifContainer = document.querySelector('.gif-container');
+//captura de evento en la lupa
 let searchBtn = document.getElementById('searchBtnId');
-let verMasBtn = document.getElementById('ver-mas-btn');
+searchBtn.addEventListener('click', (event) => {
+    newSearchBtnRequest();
+    removeContainer();
+})
 
-searchBtn.addEventListener('click', searchBtnRequest());
-
+//evento en la tecla enter
 document.querySelector('.searchbar-input').addEventListener('keyup', (event) => {
     if (event.which === 13) {
-        searchBtnRequest();
+        newSearchBtnRequest();
+        removeContainer();
     }
 });
 
-//funcion que conecta con la API de Giphy
-function searchBtnRequest() {
-    let searchInput = document.querySelector('.searchbar-input').value;
-    //elimina los nodos hijos del contenedor al hacer otra busqueda 
+//funcion para eliminar nodos hijos al realizar nueva busqueda
+function removeContainer() {
     while (searchGifContainer.firstChild) {
         searchGifContainer.removeChild(searchGifContainer.firstChild);
-    };
-    let searchUrl = "https://api.giphy.com/v1/gifs/search?api_key=HsdndAAeztqsmgGVBlrXavpjIoeADOCf&q=" + searchInput + "&limit=12&offset=0&rating=&lang=en";
+    }
+}
+
+
+const loader = document.getElementById('loader'),
+    loaderActions = {
+        showloader: function () { loader.classList.remove("display-none") },
+        hideloader: function () { loader.classList.add("display-none") }
+    }
+
+//funcion que conecta con la API
+let searchGifContainer = document.querySelector('.gif-container'),
+    offsetCounter = 0,
+    imagesGotted = [];
+function newSearchBtnRequest() {
+    const offsetRequestIndex = offsetCounter * 12;
+    let searchInput = document.querySelector('.searchbar-input').value,
+        searchUrl = `https://api.giphy.com/v1/gifs/search?api_key=HsdndAAeztqsmgGVBlrXavpjIoeADOCf&q=${searchInput}&limit=12&offset=${offsetRequestIndex}&rating=&lang=en`;
+    loaderActions.showloader();
     fetch(searchUrl)
         .then(response => response.json())
-        .then(data => createCard(data))
+        .then(data => loadingGifs(data))
         .catch((error) => {
             console.error("Ha habido un error", error);
         })
-    showSearchTitle(searchInput);
-    verMas();
-};
+    //funcion que crea un arreglo de objetos filtrado
+    function loadingGifs(response) {
+        const wsData = response.data;
+        wsData.forEach((image) => {
+            let imageWorked = {
+                url: image.images.downsized_medium.url,
+                user: image.username,
+                title: image.title,
+                fav: false
+            }
+            imagesGotted.push(imageWorked);
+        })
+        //funcion que crea las tarjetas
+        newCreateCard();
+        function newCreateCard() {
+            for (let i = offsetRequestIndex; i < imagesGotted.length; i++) {
+                let newGif = document.createElement("img")
+                newGif.setAttribute('class', 'gif-container-child')
+                newGif.setAttribute('src', imagesGotted[i].url)
+                searchGifContainer.appendChild(newGif)
+            }
+            offsetCounter += 1;
+            setTimeout(() => loaderActions.hideloader(), 2000)
+        }
+    }
+    verMasGifs();
+    showSearchTitle(searchInput)
+}
 
-//funcion que muestra automaticamente los trending gifs
-(function trendingGif() {
-    let trendingUrl = "https://api.giphy.com/v1/gifs/trending?api_key=HsdndAAeztqsmgGVBlrXavpjIoeADOCf&limit=20&rating=";
-    fetch(trendingUrl)
-        .then(response => response.json())
-        .then(data => createTrendingCard(data))
-        .catch((error) => {
-            console.error("Ha habido un error", error);
-        })
-})();
+//funcion que agrega botón "ver más"
+let verMasBtn = document.getElementById('ver-mas-btn');
+function verMasGifs() {
+    if (verMasBtn.firstChild) {
+        verMasBtn.removeChild;
+    }
+    else {
+        //crea el boton y le agrega el formato 
+        verMasBtn.classList.add('vermas-btn')
+        let verMastxt = document.createElement('h2');
+        verMastxt.textContent = "VER MÁS";
+        verMasBtn.appendChild(verMastxt);
+    }
+}
 
 //funcion que muestra el texto ingresado
 function showSearchTitle(input) {
@@ -91,46 +138,54 @@ function showSearchTitle(input) {
     return searchTitleContainer.innerHTML = "<h2 class=search-title-container>" + searchStringCapitalized + "</h2>";
 };
 
-//funcion que agrega botón "ver más"
-function verMas() {
-    if (verMasBtn.firstChild) {
-        verMasBtn.removeChild;
-    }
-    else {
-        verMasBtn.classList.add('vermas-btn')
-        let verMastxt = document.createElement('h2');
-        verMastxt.textContent = "VER MÁS";
-        verMasBtn.appendChild(verMastxt);
+//funcion que conecta con la API para las search suggestions
+let dataList = document.getElementById('autocomplete-datalist')
+function autocompleteRequest(event) {
+    const rootEvent = event.target,
+        searchInputValue = rootEvent.value
+    let autoCompleteUrl = `https://api.giphy.com/v1/tags/related/${searchInputValue}?api_key=HsdndAAeztqsmgGVBlrXavpjIoeADOCf`;
+    fetch(autoCompleteUrl)
+        .then(response => response.json())
+        .then(data => autoFill(data))
+        .catch((error) => {
+            console.error("Ha habido un error", error);
+        })
+    function autoFill(response) {
+        const requestResponse = response.data
+        requestResponse.forEach((label) => {
+            let optionElement = document.createElement('option')
+            optionElement.setAttribute("value", label.name)
+            dataList.appendChild(optionElement)
+        })
     }
 }
 
-//funcion que muestra los gifs 
-function createCard(value) {
-    let imageURL = value.data;
-    imageURL.forEach((image) => {
-        let srcImage = image.images.downsized.url;
-        searchGifContainer.innerHTML += "<img src=\"" + srcImage + "\" class=\"gif-container-child\">";
-    });
-};
-
-//funcion que crea las tarjetas
-function createTrendingCard(value) {
-    let imageURL = value.data;
-    imageURL.forEach((image) => {
-        let srcImage = image.images.downsized_medium.url;
-        let trendingGifContainer = document.getElementById("content");
-        trendingGifContainer.innerHTML += "<img src=\"" + srcImage + "\" class=\"item\">";
-    });
-}
-
+//funcion que muestra automaticamente los trending gifs
+(function trendingGif() {
+    let trendingUrl = "https://api.giphy.com/v1/gifs/trending?api_key=HsdndAAeztqsmgGVBlrXavpjIoeADOCf&limit=25&rating=";
+    fetch(trendingUrl)
+        .then(response => response.json())
+        .then(data => createTrendingCard(data))
+        .catch((error) => {
+            console.error("Ha habido un error", error);
+        })
+    //funcion que crea las tarjetas trending
+    function createTrendingCard(value) {
+        let imageURL = value.data;
+        imageURL.forEach((image) => {
+            let srcImage = image.images.downsized_medium.url;
+            let trendingGifContainer = document.getElementById("content");
+            trendingGifContainer.innerHTML += "<img src=\"" + srcImage + "\" class=\"item\">";
+        });
+    }
+})();
 
 /*funcionalidad de scroll a la seccion de trending gifs*/
-const gap = 16;
-
 const carousel = document.getElementById("carousel"),
     content = document.getElementById("content"),
     next = document.getElementById("next"),
-    prev = document.getElementById("prev");
+    prev = document.getElementById("prev"),
+    gap = 16;
 
 next.addEventListener("click", e => {
     carousel.scrollBy(width + gap, 0);
@@ -153,8 +208,4 @@ prev.addEventListener("click", e => {
 
 let width = carousel.offsetWidth;
 window.addEventListener("resize", e => (width = carousel.offsetWidth));
-/*-------------------------------------------*/
 
-
-/* agregar clase en un if cuando no haya resultados
-    searchGifContainer.classList.add("gif-container-margin"); */
